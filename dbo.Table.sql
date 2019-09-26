84418d3bdd4a937fbd7d69b5ea4e109db6edb838
abgsatman <abgsatman@hotmail.com>
2019-09-26 23:37:09 +0300

    table deleted from local path.

--- dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [FirstName] NVARCHAR(50) NULL, 
-    [LastName] NVARCHAR(50) NULL, 
-    [MobilePhone] NCHAR(10) NULL, 
-    [Email] NVARCHAR(50) NULL
-)
